b830dcaacff6330fed7dd0f88e53d127219af785
Kira-sempai <kira-sempai@narod.ru>
2017-06-13 14:51:26 +0300

    src/apps/httpd/fsdata_custom.c: remove dummy struct
    you can just use #define FS_ROOT NULL instead

--- src/apps/httpd/fsdata_custom.c
+++ src/apps/httpd/fsdata_custom.c
@@ -5,18 +5,6 @@
  *      Author: SmartWebProgrammer
  */
 
-#define file_NULL (struct fsdata_file *) NULL
-
-static const unsigned char data__index_html[] = {0x00};
-
-const struct fsdata_file file__index_html[] = { {
-file_NULL,
-data__index_html,
-NULL,
-0,
-0,
-}};
-
-#define FS_ROOT file__index_html
+#define FS_ROOT NULL
 
 
